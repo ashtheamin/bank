eff6764c1df54c5f60d0d19faa84defeb909a092
Ash Amin <ashtheamin@gmail.com>
2023-02-20 10:12:57 +1100

    Add balance updating

--- src/database.py
+++ src/database.py
@@ -238,6 +238,31 @@ def databaseAccountNew(userID, balance):
 
     return account
 
+# Update account balance
+def databaseAccountBalanceUpdateByID(accountID, balance):
+    sql = """UPDATE accounts SET balance=(%s) WHERE accountID=(%s);"""
+    conn = None
+
+    try:
+        # read database configuration
+        params = databaseConfig()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, (balance,accountID,))
+
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
 if __name__ == '__main__':
     databaseInit()
     databaseUserNew("Ash")
