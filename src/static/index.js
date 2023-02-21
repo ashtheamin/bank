f7c1d6b59ce84564f4871816336d4799a4bcde68
Ash Amin <ashtheamin@gmail.com>
2023-02-21 18:48:13 +1100

    Add registration form.

--- src/static/index.js
+++ src/static/index.js
@@ -29,13 +29,86 @@ function renderLoginForm() {
     submit.setAttribute("type", "submit");
     submit.setAttribute("class", "submit")
 
+    const register = document.createElement("button");
+    register.setAttribute("style", "display: inline-block;")
+    register.textContent = "Register a new user"
+    register.addEventListener("click", function() {
+        localStorage.setItem("registrationRequested", "true")
+        location.reload()
+    })
+    
     loginForm.appendChild(header);
     loginForm.appendChild(emailLabel);
     loginForm.appendChild(email);
     loginForm.appendChild(passwordLabel);
     loginForm.appendChild(password);
     loginForm.appendChild(submit);
+
     document.getElementsByTagName("body")[0].appendChild(loginForm);
+    document.getElementsByTagName("body")[0].appendChild(document.createElement("div"));
+    document.getElementsByTagName("body")[0].appendChild(register);
+}
+
+function renderRegistrationForm() {
+    const registrationForm = document.createElement("form");
+    registrationForm.setAttribute("method", "post");
+    registrationForm.setAttribute("action", "/recieveUserregistrationForm");
+    registrationForm.setAttribute("style", "display: inline-block;")
+
+    const header = document.createElement("h4");
+    header.textContent = "Register to Bank";
+
+    const nameLabel = document.createElement("label");
+    nameLabel.textContent = "Name"
+    const name = document.createElement("input");
+    name.setAttribute("type", "text");
+    name.setAttribute("name", "name");
+    name.setAttribute("id", "name");
+    name.setAttribute("required", "true");
+    name.setAttribute("autocomplete", "current-name")
+
+    const emailLabel = document.createElement("label");
+    emailLabel.textContent = "Email"
+    const email = document.createElement("input");
+    email.setAttribute("type", "text");
+    email.setAttribute("name", "email");
+    email.setAttribute("id", "email");
+    email.setAttribute("required", "true");
+    email.setAttribute("autocomplete", "current-email")
+
+    const passwordLabel = document.createElement("label");
+    passwordLabel.textContent = "Password";
+    const password = document.createElement("input");
+    password.setAttribute("type", "password");
+    password.setAttribute("name", "password");
+    password.setAttribute("id", "password");
+    password.setAttribute("required", "true")
+    password.setAttribute("autocomplete", "current-password")
+
+    const submit = document.createElement("input");
+    submit.setAttribute("type", "submit");
+    submit.setAttribute("class", "submit")
+
+    const returnToLogin = document.createElement("button");
+    returnToLogin.textContent = "Return To Login"
+    returnToLogin.addEventListener("click", function() {
+        localStorage.setItem("registrationRequested", "false");
+        location.reload()
+    })
+    
+    
+    registrationForm.appendChild(header);
+    registrationForm.appendChild(nameLabel);
+    registrationForm.appendChild(name);
+    registrationForm.appendChild(emailLabel);
+    registrationForm.appendChild(email);
+    registrationForm.appendChild(passwordLabel);
+    registrationForm.appendChild(password);
+    registrationForm.appendChild(submit);
+
+    document.getElementsByTagName("body")[0].appendChild(registrationForm);
+    document.getElementsByTagName("body")[0].appendChild(document.createElement("div"));
+    document.getElementsByTagName("body")[0].appendChild(returnToLogin);
 }
 
 // Get cookie function from StackOverFlow by allenhwkim
@@ -57,7 +130,13 @@ async function validateToken() {
 // Wait for the login token to be validated, then render UI.
 validateToken().then( function() {
     if (getCookie('jwtValid') != "true") {
-        renderLoginForm();
+        if (localStorage.getItem("registrationRequested") != "true") {
+            renderLoginForm();
+        }
+        else {
+            renderRegistrationForm();
+        }
+        
     }
     
     else {
