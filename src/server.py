ff84c3cadb2f9dca0b5ebc1e829fcc9680a483cd
Ash Amin <ashtheamin@gmail.com>
2023-02-21 13:34:54 +1100

    Add cookie sending and form

--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 
 app = Flask(__name__, static_url_path=("/static"))
 
@@ -6,7 +6,14 @@ app = Flask(__name__, static_url_path=("/static"))
 def index():
     return app.send_static_file("index.html")
 
+@app.route('/setBrowserLoginCookie', methods=['GET', 'POST'])
+def setBrowserLoginCookie():
+    response = make_response(app.send_static_file("index.html"))
+    response.set_cookie('jwt', 'jaydoubleyouteefromserver')
+    return response
+
 @app.route('/recieveUserLoginForm', methods=['POST'])
 def recieveUserLoginForm():
-    print(request.form['userName']) #type: ignore
+    print(request.form['email']) #type: ignore
+    print(request.cookies.get('jwt')) #type: ignore
     return app.send_static_file("index.html")
--- src/static/index.js
+++ src/static/index.js
@@ -2,18 +2,40 @@ function renderLoginForm() {
     const loginForm = document.createElement("form");
     loginForm.setAttribute("method", "post");
     loginForm.setAttribute("action", "/recieveUserLoginForm");
+    loginForm.setAttribute("style", "display: inline-block;")
 
-    const userName = document.createElement("input");
-    userName.setAttribute("type", "text");
-    userName.setAttribute("name", "userName");
-    userName.setAttribute("id", "userName");
+    const header = document.createElement("h4");
+    header.textContent = "Login to Bank";
+
+    const emailLabel = document.createElement("label");
+    emailLabel.textContent = "Email"
+    const email = document.createElement("input");
+    email.setAttribute("type", "text");
+    email.setAttribute("name", "email");
+    email.setAttribute("id", "email");
+    email.setAttribute("autocomplete", "current-email")
+
+    const passwordLabel = document.createElement("label");
+    passwordLabel.textContent = "Password";
+    const password = document.createElement("input");
+    password.setAttribute("type", "password");
+    password.setAttribute("name", "password");
+    password.setAttribute("id", "password");
+    password.setAttribute("autocomplete", "current-password")
 
     const submit = document.createElement("input");
     submit.setAttribute("type", "submit");
+    submit.setAttribute("class", "submit")
 
-    loginForm.appendChild(userName);
+    loginForm.appendChild(header);
+    loginForm.appendChild(emailLabel);
+    loginForm.appendChild(email);
+    loginForm.appendChild(passwordLabel);
+    loginForm.appendChild(password);
     loginForm.appendChild(submit);
     document.getElementsByTagName("body")[0].appendChild(loginForm);
 }
 
 renderLoginForm();
+
+console.log(document.cookie)
